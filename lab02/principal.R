84ee4f487de799e4bd9035e873a55eb598e121a2
Freguglia <victorfreguglia@gmail.com>
2023-09-08 20:48:46 -0300

    Ajustes script lab02

--- lab02/principal.R
+++ lab02/principal.R
@@ -1,7 +1,7 @@
 configuracoes <- yaml::read_yaml("configuracoes.yaml")
 
 purrr::map(configuracoes$arquivos, function(x) {
-  read_json(file.path("dados", x))
+             jsonlite::read_json(file.path("dados", x))
 }) -> lidos
 
 listao <- do.call(c, args = lidos)
@@ -31,11 +31,11 @@ tabelao %>%
   mutate(diferenca = horario - anterior) %>%
   filter(diferenca > 8*60*60) -> horario_validos
 
-tabelao %>%
-  filter(horario < horario_validos$anterior[1] |
-           horario > horario_validos$horario[1]) %>%
-  filter(horario < horario_validos$anterior[2] |
-           horario > horario_validos$horario[2]) -> tabelao_bom
+tabelao_bom <- tabelao %>%
+  group_by(horario) %>%
+  filter(all(!(horario > horario_validos$anterior &
+                 horario < horario_validos$horario))) %>%
+  ungroup()
 
 combin <- expand.grid(v1 = c(T,F), v2 = c(T,F), v3 = c(T,F), v4 = c(T,F))
 l <- unlist(apply(combin, 1, list), recursive = FALSE)
