08f4458fbf9c1be52e22e3ad742f4f87f4a5f940
Freguglia <victorfreguglia@gmail.com>
2023-10-06 18:17:34 -0300

    Solução com verdadeiro/falso em yaml

--- prova/provaXXXXXX/R/respostas.R
+++ prova/provaXXXXXX/R/respostas.R
@@ -1,29 +1,7 @@
 #' @export
 respostas <- function() {
-  list(
-    list(verdadeiro = TRUE),
-
-    list(verdadeiro = FALSE,
-         justificativa = paste(
-           "Embora seja uma boa prática tentar manter uma função",
-           "por arquivo, isso não é um requerimento e podemos",
-           "definir múltiplos objetos no mesmo arquivo .R.",
-           "Um caso comum é definir funções auxiliares junto com",
-           "uma função principal, sendo essas funções auxiliares",
-           "exportadas ou não.",
-           sep = " ")
-    ),
-
-    list(verdadeiro = TRUE),
-
-    list(verdadeiro = FALSE,
-         justificativa = paste(
-           "A relação não precisa ser um pra um, pois podemos",
-           "documentar múltiplas funções no memso tópico utilizando,",
-           "por exemplo, as tags do Roxygen @describeIn e @rdname.",
-           "Além disso, não existe requerimento de que todas as funções",
-           "sejam documentadas (estejam descritar em um arquivo .Rd).",
-           sep = " ")
-    )
-  )
+  yaml_respostas <- system.file("extdata",
+                                "respostas.yaml",
+                                package = "provaXXXXXX")
+  return(yaml::read_yaml(yaml_respostas))
 }
